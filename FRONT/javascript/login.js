69268e90e2886d5729cbd3aac401e8493929862f
Santiago Bertorello <santinbertorello@gmail.com>
2023-11-03 00:15:03 -0300

    cambios

--- FRONT/javascript/login.js
+++ FRONT/javascript/login.js
@@ -138,7 +138,7 @@ function loguear() {
                     text="¡Inicio de sesión exitoso!",
                     icon="success",
                 );
-                modal.style.display = 'none'; 
+                myModal.style.display = 'none'; 
                 setTimeout(function() {
                     }, 4000);
                 
